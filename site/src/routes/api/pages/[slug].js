afd55e32ddca198931aa162bce2f1de4a81aa0f7
Ken Jones <kenjwho@gmail.com>
2020-11-04 22:18:10 -0500

    update Link component

--- site/src/routes/api/pages/[slug].js
+++ site/src/routes/api/pages/[slug].js
@@ -1,28 +1,26 @@
-import client from '../../../sanityClient'
+import client from "../../../sanityClient";
 
-export async function get (req, res) {
-  try {
-    // We have access to req.params.slug because the filename has [slug] in it.
-    const { slug } = req.params;
-    // if (req.params === 'thiIsTheHomePage')
-    const pageFilter = '*[][0]';
-    const pageProjection = `{
+export async function get(req, res) {
+	try {
+		// We have access to req.params.slug because the filename has [slug] in it.
+		const { slug } = req.params;
+		const pageFilter = "*[][0]";
+		const pageProjection = `{
       'page': *[pageInfo.slug.current == "${slug}"]{...}
     }`;
-    
-    const pageQuery = pageFilter + pageProjection;
-    const pageData = await client.fetch(pageQuery, { slug })
-    
 
-    res.end(JSON.stringify({ pageData }));
-  } catch (err) {
-    console.log('err:', err.message)
-    res.writeHead(500, {
-      'Content-Type': 'application/json'
-    });
+		const pageQuery = pageFilter + pageProjection;
+		const pageData = await client.fetch(pageQuery, { slug });
 
-    res.end(JSON.stringify({
-      message: err.message
-    }));  
-  }
-};
+		res.end(JSON.stringify({ pageData }));
+	} catch (err) {
+		console.log("err:", err.message);
+		res.writeHead(500, {
+			"Content-Type": "application/json",
+		});
+
+		res.end(JSON.stringify({
+			message: err.message,
+		}));
+	}
+}
